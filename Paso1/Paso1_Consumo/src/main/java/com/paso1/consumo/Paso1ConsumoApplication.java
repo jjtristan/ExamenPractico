1b7ed38e939b6b0041489d0e1c6eaa5fb8739e47
Juan Tristan <jdjgomezt@gmail.com>
2025-02-13 22:24:55 -0600

    Creacion de proyecto primer paso

---
+++ Paso1/Paso1_Consumo/src/main/java/com/paso1/consumo/Paso1ConsumoApplication.java
@@ -0,0 +1,13 @@
+package com.paso1.consumo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Paso1ConsumoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Paso1ConsumoApplication.class, args);
+	}
+
+}
---
+++ Paso1/Paso1_Consumo/src/test/java/com/paso1/consumo/Paso1ConsumoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paso1.consumo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Paso1ConsumoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
