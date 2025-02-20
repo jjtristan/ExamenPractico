178bdd4a381a734b5e656422a3979de3cd47b49d
Juan Tristan <jdjgomezt@gmail.com>
2025-02-19 23:09:48 -0600

    configuraciones de pruebas

--- Paso1/Paso2_Puente/src/test/java/com/paso2/puente/Paso2PuenteApplicationTests.java
+++ Paso1/Paso2_Puente/src/test/java/com/paso2/puente/Paso2PuenteApplicationTests.java
@@ -1,12 +1,12 @@
 package com.paso2.puente;
 
-
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class Paso2PuenteApplicationTests {
 
-	@org.junit.Test
+	@Test
 	void contextLoads() {
 	}
 
